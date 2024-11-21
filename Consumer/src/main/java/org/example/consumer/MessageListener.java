af39bb1ef79c1946325ce65e321b3da0066fa0cb
douaa <douaa.code0@gmail.com>
2024-11-21 17:23:18 +0100

    first commit

---
+++ Consumer/src/main/java/org/example/consumer/MessageListener.java
@@ -0,0 +1,13 @@
+package org.example.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageListener {
+
+    @RabbitListener(queues = "myQueue")
+    public void receiveMessage(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
